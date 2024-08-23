bd6abbf5324d027fd8add1fe776d9b42404eac92
lulzshadowwalker <lulzshadowwalker@gmail.com>
2024-08-23 17:24:55 +0300

    HUD

--- main.gd
+++ main.gd
@@ -5,8 +5,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	new_game()
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -16,17 +15,24 @@ func _process(delta: float) -> void:
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$EnemyTimer.stop()
+	
+	$HUD.show_game_over()
+	get_tree().call_group("enemies", "queue_free")
 
 
 func new_game() -> void:
 	score = 0
 	$Player.start($StartPosition.position)
+	$HUD.show_message("Hmm")
 	$StartTimer.start()
 
 
 func _on_start_timer_timeout() -> void:
 	$ScoreTimer.start()
 	$EnemyTimer.start()
+	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 
 func _on_enemy_timer_timeout() -> void:
@@ -50,3 +56,4 @@ func _on_enemy_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
